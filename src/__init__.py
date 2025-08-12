f6d71e300f113b2ed07699ce33225d41a38c604f
Keshy31 <keshavmohabir@gmail.com>
2025-08-12 12:08:31 +0200

    Directory setup

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+EntangleRL: Reinforcement Learning for Quantum State Preparation
+
+A project that combines quantum computing simulation with reinforcement learning
+to train agents that can prepare quantum states like Bell pairs through
+sequences of quantum gate operations.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Keshav Mohabir"
+__email__ = "keshav@catdesign.ltd"
+
+# Core modules will be imported here as they're developed
+# from .environment import QuantumPrepEnv
+# from .training import PPOTrainer  
+# from .visualization import QuantumVisualizer
